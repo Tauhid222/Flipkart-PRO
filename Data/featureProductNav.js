2ef0f8336c961b4325a03fed6798d90dcaa39d83
Tauhid222 <152008312+Tauhid222@users.noreply.github.com>
2024-05-21 21:26:13 +0530

    first commit

---
+++ Data/featureProductNav.js
@@ -0,0 +1,62 @@
+export const featureProductNav = [
+    {
+        id : "featureProductNav_1",
+        img : "https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+        name : "Grocery",
+       
+    },
+    {
+        id : "featureProductNav_2",
+        img : "https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100",
+        name : "Mobiles",
+        
+    },
+    {
+        id : "featureProductNav_3",
+        img : "https://rukminim1.flixcart.com/flap/128/128/image/c12afc017e6f24cb.png?q=100",
+        name : "Fashion",
+        
+    },
+    {
+        id : "featureProductNav_4",
+        img : "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
+        name : "Electronics",
+       
+    },
+    {
+        id : "featureProductNav_5",
+        img : "https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100",
+        name : "Home",
+       
+    },
+    {
+        id : "featureProductNav_6",
+        img : "https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100",
+        name : "Appliances",
+       
+    },
+    {
+        id : "featureProductNav_7",
+        img : "https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100",
+        name : "Travel",
+       
+    },
+    {
+        id : "featureProductNav_8",
+        img : "https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100",
+        name : "Top Offers",
+        
+    },
+    {
+        id : "featureProductNav_9",
+        img : "https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100",
+        name : "Beauty, Toys & More",
+       
+    },
+    {
+        id : "featureProductNav_9",
+        img : "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/05d708653beff580.png?q=100",
+        name : "2-Wheelers",
+        
+    }
+]
---
+++ Data/imageSlider.js
@@ -0,0 +1,22 @@
+export const imageSlider = [
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/68dad970da9c63c7.jpg?q=50",
+        
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/6c93de92458aa0c1.jpeg?q=50",
+        
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/d02b3b49e4150abb.jpg?q=50",
+       
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/c0bec65432fb07fc.jpg?q=50",
+      
+    },
+    {
+        img : "https://rukminim1.flixcart.com/flap/1688/280/image/75a15c3e19c3f7de.jpg?q=50",
+       
+    }
+]
